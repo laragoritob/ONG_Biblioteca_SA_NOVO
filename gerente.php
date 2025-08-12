44e10ace7199bd2eca0c73c2fe29611cad0516ad
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-08-12 17:05:05 -0300

    versao 1

---
+++ gerente.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head> 
+         <meta charset="UTF-8">
+         <title> ONG Bilbioteca - Gerente </title>
+         <link rel ="stylesheet" type="text/css" href="css/telgerente.css"/>
+         <script src="javascript/JS_Logout.js" defer></script>
+    </head>
+    <body> 
+        <a href="#" class="logout-btn">🚶🏻‍♂️ Logout</a>
+        <header> 
+            <h1> Bem-Vindo, "Gerente"! </h1>
+            <div>
+                <img src="img/logo_trans.png" title="imgs" class="logo"> 
+            </div>
+        </header>
+        <ul class="nav-bar">
+            <li><a href="#"> Início </a></li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn"> Funcionários </a>
+                <div class="dropdown-content">
+                    <a href="#"> Registrar Funcionário </a>
+                    <a href="#"> Consultar Funcionários </a>
+                </div>
+            </li>
+
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn"> Clientes </a>
+                <div class="dropdown-content">
+                    <a href="#"> Registrar Cliente </a>
+                    <a href="#"> Consultar Clientes </a>
+                </div>
+            </li>
+
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn"> Livros </a>
+                <div class="dropdown-content">
+                    <a href="#"> Adicionar Livro </a>
+                    <a href="#"> Consultar Livros </a>
+                </div>
+            </li>
+
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn"> Empréstimos </a>
+                <div class="dropdown-content">
+                    <a href="#"> Registrar Empréstimo </a>
+                    <a href="#"> Consultar Empréstimos </a>
+                </div>
+            </li>
+        </ul>
+    </body>
+    </html>
--- login.php
+++ login.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ONG Biblioteca - Sala Arco-íris</title>
     <link rel="stylesheet" type="text/css" href="css/login.css">
-    <script src="javascript/JS_Login.js" defer></script>
 </head>
 <body>
     <div class="container">
