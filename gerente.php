6879917c909147cceda1106b4c2f4d4e157ccb19
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-08-27 13:59:00 -0300

    Versão 5

--- gerente.php
+++ gerente.php
@@ -3,7 +3,7 @@
     require_once 'conexao.php';
 
     if (!isset($_SESSION['usuario'])) {
-        header('Location: login.php');
+        header('Location: index.php');
         exit();
     }
 ?>
