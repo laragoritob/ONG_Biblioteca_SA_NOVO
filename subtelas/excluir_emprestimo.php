af19e65698ca07eeea50790df2685ef3af437264
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-08-29 17:07:10 -0300

    Excluir Empréstimo
    Tela para excluir empréstimos

--- subtelas/excluir_emprestimo.php
+++ subtelas/excluir_emprestimo.php
@@ -11,14 +11,14 @@ if (isset($_GET['id'])) {
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 
         if ($stmt->execute()) {
-            echo "<script>alert('Emprestimo excluído com sucesso!'); window.location.href='consultar_emprestimo.php';</script>";
+            echo "<script>alert('Empréstimo excluído com sucesso!'); window.location.href='consultar_emprestimo.php';</script>";
         } else {
-            echo "<script>alert('Erro ao excluir emprestimo.'); window.location.href='consultar_emprestimo.php';</script>";
+            echo "<script>alert('Erro ao excluir empréstimo.'); window.location.href='consultar_emprestimo.php';</script>";
         }
     } catch (PDOException $e) {
         echo "Erro: " . $e->getMessage();
     }
 } else {
-    echo "<script>alert('ID do emprestimo não informado!'); window.location.href='consultar_emprestimo.php';</script>";
+    echo "<script>alert('ID do empréstimo não informado!'); window.location.href='consultar_emprestimo.php';</script>";
 }
 ?>
