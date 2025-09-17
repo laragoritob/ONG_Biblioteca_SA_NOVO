238aaa3b0c7a65a890b44c49d6c8c1889261b7fb
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-09-17 14:00:14 -0300

    Teste Livro

--- subtelas/teste_python/teste_livro.py
+++ subtelas/teste_python/teste_livro.py
@@ -19,62 +19,62 @@ driver.find_element(By.CSS_SELECTOR, "button[type='submit']"). click()
 time.sleep(1)
 
 # Navega para a página de Fazer Empréstimo
-driver.get("http://localhost:8080/ONG_Biblioteca_SA_NOVO/subtelas/registrar_autor.php")
+driver.get("http://localhost:8080/ONG_Biblioteca_SA_NOVO/subtelas/registrar_livro.php")
 
 time.sleep(1)
 
 # Preenche o campo Id do autor
-autor_input = driver.find_element(By.ID, "titulo")
+titulo_input = driver.find_element(By.ID, "titulo")
+titulo_input.clear()
+titulo_input.send_keys("Os Mais Mais++")
+time.sleep(1)
+
+# Preenche o campo id do autor
+autor_input = driver.find_element(By.ID, "autor")
 autor_input.clear()
-autor_input.send_keys("Os Mais Mais++")
-time.sleep(2)
+autor_input.send_keys("Machado de Assis")
+time.sleep(1)
 
-# Preenche o campo id do telefone
-telefone_input = driver.find_element(By.ID, "nome_autor")
-telefone_input.clear()
-telefone_input.send_keys("Emanuel da Silva")
-time.sleep(2)
+# Preenche o campo id do autor
+lancamento_input = driver.find_element(By.ID, "data_lancamento")
+lancamento_input.clear()
+lancamento_input.send_keys("14111999")
+time.sleep(1)
 
-# Preenche o campo id do telefone
-email_input = driver.find_element(By.ID, "data_lancamento")
-email_input.clear()
-email_input.send_keys("14-11-1999")
-time.sleep(2)
-
-perfil_select = Select(driver.find_element(By.ID, "genero"))
-perfil_select.select_by_value("2")
-time.sleep(2)
+genero_select = Select(driver.find_element(By.ID, "cod_genero"))
+genero_select.select_by_value("2")
+time.sleep(1)
 
 # Preenche o campo Id do autor
-autor_input = driver.find_element(By.ID, "nome_editora")
-autor_input.clear()
-autor_input.send_keys("Editora Objetiva")
-time.sleep(2)
+editora_input = driver.find_element(By.ID, "nome_editora")
+editora_input.clear()
+editora_input.send_keys("Editora Objetiva")
+time.sleep(1)
 
 # Preenche o campo Id do autor
-autor_input = driver.find_element(By.ID, "num_prateleira")
-autor_input.clear()
-autor_input.send_keys("9")
-time.sleep(2)
+prateleira_input = driver.find_element(By.ID, "num_prateleira")
+prateleira_input.clear()
+prateleira_input.send_keys("9")
+time.sleep(1)
 
 # Preenche o campo Id do autor
-autor_input = driver.find_element(By.ID, "quantidade")
-autor_input.clear()
-autor_input.send_keys("14")
-time.sleep(2)
+qntd_input = driver.find_element(By.ID, "quantidade")
+qntd_input.clear()
+qntd_input.send_keys("14")
+time.sleep(1)
 
 # Preenche o campo Id do autor
-autor_input = driver.find_element(By.ID, "nome_doador")
-autor_input.clear()
-autor_input.send_keys("Frank Ocean")
-time.sleep(2)
+doador_input = driver.find_element(By.ID, "nome_doador")
+doador_input.clear()
+doador_input.send_keys("Frank Ocean")
+time.sleep(1)
 
 # Clica no botão de cadastro
 cadastro_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 cadastro_button.click()
 
 # Aguarda alguns segundos para verificar o resultado
-time.sleep(4)
+time.sleep(3)
 
 # Fecha o navegador
 driver.quit()
